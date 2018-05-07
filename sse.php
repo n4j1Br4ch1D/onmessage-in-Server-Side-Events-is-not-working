4d125a7b97046d77965b0b5eb19cb06b1e69f92c
n4j1Br4ch1D <38995898+n4j1Br4ch1D@users.noreply.github.com>
2018-05-06 19:19:06 -0700

    Update sse.php

--- sse.php
+++ sse.php
@@ -3,16 +3,3 @@
 header("Content-Type: text/event-stream");
 echo 'data: Received At ' . date("Y/m/d h:i:sa") . "\n\n";
 flush();
-
-
-
-
-/*
-
-The Problem is With ServerSideCode:
-1-make sure you add correct headers such as : header("Content-Type: text/event-stream");.
-2-make sure you set your response to data like this: 'data: your response text'.
-3-make sure you add a line break to the end of response text twise like this: "\n\n";.
-https://github.com/n4j1Br4ch1D
-
-*/
